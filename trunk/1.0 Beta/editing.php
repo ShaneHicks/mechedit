8b82c262cd943c1f3b4bba8a5c97796ce64e1701
rwmech <rwmech@520e625c-3a4e-11de-97fb-aff7d2a6cc54>
2009-05-07 19:35:53 +0000

    Moved rounded styling into theme instead of in source code.  Not a required component for operation.

--- trunk/1.0 Beta/editing.php
+++ trunk/1.0 Beta/editing.php
@@ -1,27 +1,15 @@
 <?php
 
 /*******************************************************************************
-
 Mech Edit Copyright 2009 Robert W. Mech
-
  ($Rev: 0 $)
-
 Website: http://www.mechedit.com/
-
 Author: Robert W. Mech
 
-
-
 Mech Edit is available for use in all personal or commercial projects under both 
-
 MIT and GPL licenses. This means that you can choose the license that best suits 
-
 your project, and use it accordingly.
-
-
-
 Redistributions of files must retain the above copyright notice.
-
 *******************************************************************************/
 
 require_once 'authentication.php';
@@ -29,9 +17,7 @@ require_once 'authentication.php';
 include 'siteManager.php';
 
 /*
-
  * Main Output Function
-
  */
 
 function mainContent(){
@@ -73,95 +59,41 @@ function mainContent(){
 }
 
 /*
-
  * Include all scripts necessary
-
  */
 
 function scriptIncludes(){
 
 	// jQuery and jQuery Corners
-
 	echo '<script src="jscripts/jquery/jquery-1.3.2.min.js"></script>'."\n";
-
-	echo '<script src="jscripts/jquery/jquery.corner.js"></script>'."\n";
-
 	if($_GET[q]=='sites'){ // Site Manager Script
-
 		echo '<script src="jscripts/mechedit/siteManager.js"></script>'."\n";
-
 		echo '<script src="jscripts/mechedit/pageManager.js"></script>'."\n";
-
 	}
 
 	if($_GET[q]=='edit'){ // Site Manager Script
-
 		echo '<script type="text/javascript" src="jscripts/tiny_mce/tiny_mce.js"></script>'."\n";
-
 		echo '<script src="jscripts/other/json2.js"></script>'."\n";
-
 		echo '<script src="jscripts/mechedit/editManager.js"></script>'."\n";
-
-		
-
 	}
-
 }
-
-/*
-
- * Activate Javascript in footer
-
- */
-
-function scriptActivation(){
-
-	echo '
-
-		<script>
-
-		$(document).ready(function(){
-			$("#wrapper").corner("20px");	
-		});
-
-		</script>	
-
-	';	
-
-}
-
-
 
 function buildLinks(){
 
 	processLogin();
-
 	// Admins can access site administration, every valid user can access page editing.
-
 	?>
-
 	      <ul>
-
 	        <li><a href="index.php"><?=TXT_HOME?></a></li>
-
 <?php if(isset($_SESSION['role']) && $_SESSION['role']=='admin'): ?>
-
 	        <li><a href="index.php?q=sites"><?=TXT_SITES?></a></li>
-
 <?php endif ?>
-
 <?php if(isset($_SESSION['role'])): ?>
-
 	        <li><a href="index.php?q=edit"><?=TXT_EDITOR?></a></li>
-
 	        <li><a href="index.php?q=logout"><?=TXT_LOGOUT?></a></li>
-
 <?php endif ?>
-
 	      </ul>
-
 	<?php
-
 }
 
 
--- trunk/1.0 Beta/themes/basic/footer.php
+++ trunk/1.0 Beta/themes/basic/footer.php
@@ -1,38 +1,33 @@
 <?php
 
 /*******************************************************************************
-
-Mech Edit Copyright 2009 Robert W. Mech
-
+ Mech Edit Copyright 2009 Robert W. Mech
  ($Rev: 0 $)
-
-Website: http://www.mechedit.com/
-
-Author: Robert W. Mech
-
-
-
-Mech Edit is available for use in all personal or commercial projects under both 
-
-MIT and GPL licenses. This means that you can choose the license that best suits 
-
-your project, and use it accordingly.
-
-
-
-Redistributions of files must retain the above copyright notice.
+ Website: http://www.mechedit.com/
+ Author: Robert W. Mech
+ Mech Edit is available for use in all personal or commercial projects under both
+ MIT and GPL licenses. This means that you can choose the license that best suits
+ your project, and use it accordingly.
+ Redistributions of files must retain the above copyright notice.
 
 *******************************************************************************/
 
 ?>
-
-	  <div id="footer"> <?=COMPANY_NAME?> <a href="http://mechedit.mechsoftware.com/"><?=TXT_POWEREDBY?></a> | <?=VERSION_STRING?></div>
-
-	</div>
-
-  <?php scriptActivation(); ?>
-
-  </body>
-
+<div id="footer">
+    <?= COMPANY_NAME ?>
+    <a href="http://mechedit.mechsoftware.com/">
+        <?= TXT_POWEREDBY ?>
+    </a>
+    | 
+    <?= VERSION_STRING ?>
+</div>
+</div>
+<script>
+    
+    $(document).ready(function(){
+        $("#wrapper").corner("20px");
+    });
+    
+</script>
+</body>
 </html>
-
