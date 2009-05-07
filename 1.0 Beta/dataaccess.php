1f4a8571a7c9ae2cd2afd1c9d3ce3d0aeb9226cd
rwmech <rwmech@520e625c-3a4e-11de-97fb-aff7d2a6cc54>
2009-05-07 19:12:58 +0000

    Updated Website Address

--- 1.0 Beta/dataaccess.php
+++ 1.0 Beta/dataaccess.php
@@ -1,66 +1,132 @@
-<?php
-/*******************************************************************************
-Mech Edit Copyright 2009 Robert W. Mech
- ($Rev: 0 $)
-Website: http://mechedit.mechsoftware.com/
-Author: Robert W. Mech
-
-Mech Edit is available for use in all personal or commercial projects under both 
-MIT and GPL licenses. This means that you can choose the license that best suits 
-your project, and use it accordingly.
-
-Redistributions of files must retain the above copyright notice.
-*******************************************************************************/
-// Users
-function getUsers(){
-	error_reporting(0);
-	$users=file_get_contents(DATA_DIR . 'users.dat');
-	if($users===false){
-		$userData[0]=array('id'=>'0', 'site'=>'Administrator','user'=>'admin','password'=>'adminpass','role'=>'admin');
-		$userData[1]=array('id'=>'1', 'site'=>'Editor','user'=>'editor','password'=>'editorpass','role'=>'editor');
-		putUsers($userData);
-	}else{
-		$userData=unserialize($users);
-	} 
-	error_reporting(1);
-	return $userData;
-}
-function putUsers($userData){
-	file_put_contents(DATA_DIR.'users.dat',serialize($userData));
-}
-
-
-// Generic Get Sites Function
-function getSites(){
-	error_reporting(0);
-	$sites=file_get_contents(DATA_DIR . 'sites.dat');
-	if($sites===false){
-		$siteData[0]=array('id'=>'0', 'site'=>'www.example.net','user'=>'user','password'=>'password','path'=>'public_html/');
-		putSites($siteData);
-	}else{
-		$siteData=unserialize($sites);
-	} 
-	error_reporting(1);
-	return $siteData;
-}
-function putSites($siteData){
-	file_put_contents(DATA_DIR.'sites.dat',serialize($siteData));
-}
-
-// Pages 
-function getPages(){
-	error_reporting(0);
-	$pages=file_get_contents(DATA_DIR . 'pages.dat');
-	if($pages===false){
-		$pageData=array();
-	}else{
-		$pageData=unserialize($pages);
-	} 
-	error_reporting(1);
-	return $pageData;
-}
-function putPages($pageData){
-	file_put_contents(DATA_DIR.'pages.dat',serialize($pageData));
-}
-
-?>
+<?php
+
+/*******************************************************************************
+
+Mech Edit Copyright 2009 Robert W. Mech
+
+ ($Rev: 0 $)
+
+Website: http://www.mechedit.com/
+
+Author: Robert W. Mech
+
+
+
+Mech Edit is available for use in all personal or commercial projects under both 
+
+MIT and GPL licenses. This means that you can choose the license that best suits 
+
+your project, and use it accordingly.
+
+
+
+Redistributions of files must retain the above copyright notice.
+
+*******************************************************************************/
+
+// Users
+
+function getUsers(){
+
+	error_reporting(0);
+
+	$users=file_get_contents(DATA_DIR . 'users.dat');
+
+	if($users===false){
+
+		$userData[0]=array('id'=>'0', 'site'=>'Administrator','user'=>'admin','password'=>'adminpass','role'=>'admin');
+
+		$userData[1]=array('id'=>'1', 'site'=>'Editor','user'=>'editor','password'=>'editorpass','role'=>'editor');
+
+		putUsers($userData);
+
+	}else{
+
+		$userData=unserialize($users);
+
+	} 
+
+	error_reporting(1);
+
+	return $userData;
+
+}
+
+function putUsers($userData){
+
+	file_put_contents(DATA_DIR.'users.dat',serialize($userData));
+
+}
+
+
+
+
+
+// Generic Get Sites Function
+
+function getSites(){
+
+	error_reporting(0);
+
+	$sites=file_get_contents(DATA_DIR . 'sites.dat');
+
+	if($sites===false){
+
+		$siteData[0]=array('id'=>'0', 'site'=>'www.example.net','user'=>'user','password'=>'password','path'=>'public_html/');
+
+		putSites($siteData);
+
+	}else{
+
+		$siteData=unserialize($sites);
+
+	} 
+
+	error_reporting(1);
+
+	return $siteData;
+
+}
+
+function putSites($siteData){
+
+	file_put_contents(DATA_DIR.'sites.dat',serialize($siteData));
+
+}
+
+
+
+// Pages 
+
+function getPages(){
+
+	error_reporting(0);
+
+	$pages=file_get_contents(DATA_DIR . 'pages.dat');
+
+	if($pages===false){
+
+		$pageData=array();
+
+	}else{
+
+		$pageData=unserialize($pages);
+
+	} 
+
+	error_reporting(1);
+
+	return $pageData;
+
+}
+
+function putPages($pageData){
+
+	file_put_contents(DATA_DIR.'pages.dat',serialize($pageData));
+
+}
+
+
+
+?>
+
--- 1.0 Beta/pageManager.php
+++ 1.0 Beta/pageManager.php
@@ -1,69 +1,137 @@
-<?php
-/*******************************************************************************
-Mech Edit Copyright 2009 Robert W. Mech
- ($Rev: 0 $)
-Website: http://mechedit.mechsoftware.com/
-Author: Robert W. Mech
-
-Mech Edit is available for use in all personal or commercial projects under both 
-MIT and GPL licenses. This means that you can choose the license that best suits 
-your project, and use it accordingly.
-
-Redistributions of files must retain the above copyright notice.
-*******************************************************************************/
-
-include 'configure.php';
-require_once 'authentication.php';
-session_start();
-if(isset($_GET['action']) && !processLogin()){
-	echo NOT_AUTHENTICATED;
-	exit;
-}
-
-// Actions for this XHR File
-if($_GET['action']=='list')
-	listPages();
-if($_GET['action']=='add')
-	addPage();
-if($_GET['action']=='remove')
-	removePage();
-
-function removePage(){
-	$pageData=getPages();
-	$found=false;
-	for($i=0;$i<sizeof($pageData);$i++){
-		if($pageData[$i]['key']==$_GET['key']){
-			$found=true;
-			unset($pageData[$i]);
-			break;
-		}
-	}
-	if($found==true){
-		array_unshift ($pageData, array_shift ($pageData));
-		putPages($pageData);	
-		echo "{status:'".PAGE_DELETED."'}";
-	}else{
-		echo "{status:'".PAGE_NOT_FOUND."'}";
-	}
-}
-function listPages(){
-	$pageResult=array();	
-	$pageData=getPages();
-	for($i=0;$i<sizeof($pageData);$i++){
-		if($pageData[$i]['id']==$_GET['id']){
-			$pageResult[]=$pageData[$i];
-		}
-	}
-	echo json_encode($pageResult);
-}
-
-function addPage(){
-	// No "update" just add pages, delete and re-add if update necessary.
-	$pageData=getPages();
-	$pageData[]=array('id'=> $_GET['id'], 'title'=>$_GET['PAG_name'],'path'=>$_GET['PAG_path'],'key'=>uniqid());
-	putPages($pageData);
-	echo "{status:'".PAGE_ADDED."'}";
-}
-
-
+<?php
+
+/*******************************************************************************
+
+Mech Edit Copyright 2009 Robert W. Mech
+
+ ($Rev: 0 $)
+
+Website: http://www.mechedit.com/
+
+Author: Robert W. Mech
+
+
+
+Mech Edit is available for use in all personal or commercial projects under both 
+
+MIT and GPL licenses. This means that you can choose the license that best suits 
+
+your project, and use it accordingly.
+
+
+
+Redistributions of files must retain the above copyright notice.
+
+*******************************************************************************/
+
+
+
+include 'configure.php';
+
+require_once 'authentication.php';
+
+session_start();
+
+if(isset($_GET['action']) && !processLogin()){
+
+	echo NOT_AUTHENTICATED;
+
+	exit;
+
+}
+
+
+
+// Actions for this XHR File
+
+if($_GET['action']=='list')
+
+	listPages();
+
+if($_GET['action']=='add')
+
+	addPage();
+
+if($_GET['action']=='remove')
+
+	removePage();
+
+
+
+function removePage(){
+
+	$pageData=getPages();
+
+	$found=false;
+
+	for($i=0;$i<sizeof($pageData);$i++){
+
+		if($pageData[$i]['key']==$_GET['key']){
+
+			$found=true;
+
+			unset($pageData[$i]);
+
+			break;
+
+		}
+
+	}
+
+	if($found==true){
+
+		array_unshift ($pageData, array_shift ($pageData));
+
+		putPages($pageData);	
+
+		echo "{status:'".PAGE_DELETED."'}";
+
+	}else{
+
+		echo "{status:'".PAGE_NOT_FOUND."'}";
+
+	}
+
+}
+
+function listPages(){
+
+	$pageResult=array();	
+
+	$pageData=getPages();
+
+	for($i=0;$i<sizeof($pageData);$i++){
+
+		if($pageData[$i]['id']==$_GET['id']){
+
+			$pageResult[]=$pageData[$i];
+
+		}
+
+	}
+
+	echo json_encode($pageResult);
+
+}
+
+
+
+function addPage(){
+
+	// No "update" just add pages, delete and re-add if update necessary.
+
+	$pageData=getPages();
+
+	$pageData[]=array('id'=> $_GET['id'], 'title'=>$_GET['PAG_name'],'path'=>$_GET['PAG_path'],'key'=>uniqid());
+
+	putPages($pageData);
+
+	echo "{status:'".PAGE_ADDED."'}";
+
+}
+
+
+
+
+
 ?>
