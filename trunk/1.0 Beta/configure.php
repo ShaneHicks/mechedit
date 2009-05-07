6adce4a9b7b21ad87f69430cb0fa116a553abcc8
rwmech <rwmech@520e625c-3a4e-11de-97fb-aff7d2a6cc54>
2009-05-07 21:00:18 +0000

    Removed some extra lines placed into the files by a dos/unix conversion which I have no idea where or why took place.  In any case these files are updated to remove the extra line feeds.

--- trunk/1.0 Beta/configure.php
+++ trunk/1.0 Beta/configure.php
@@ -1,68 +1,41 @@
 <?php
 
 /*******************************************************************************
-
-Mech Edit Copyright 2009 Robert W. Mech
-
+ Mech Edit Copyright 2009 Robert W. Mech
  ($Rev: 0 $)
+ Website: http://www.mechedit.com/
+ Author: Robert W. Mech
 
-Website: http://www.mechedit.com/
-
-Author: Robert W. Mech
-
-
-
-Mech Edit is available for use in all personal or commercial projects under both 
-
-MIT and GPL licenses. This means that you can choose the license that best suits 
-
-your project, and use it accordingly.
+ Mech Edit is available for use in all personal or commercial projects under both
+ MIT and GPL licenses. This means that you can choose the license that best suits
+ your project, and use it accordingly.
+ Redistributions of files must retain the above copyright notice.
+ *******************************************************************************/
 
 
 
-Redistributions of files must retain the above copyright notice.
+// In the future this could be easily changed to use MYSQL instead of text files.
 
-*******************************************************************************/
-
-
-
-	// In the future this could be easily changed to use MYSQL instead of text files.
-
-	require_once ('dataaccess.php');
+require_once ('dataaccess.php');
 
 
 
-	// Appearance Settings
+// Appearance Settings
 
-	define(THEME_PATH,'themes/basic/');
+define(THEME_PATH, 'themes/basic/');
+define(COMPANY_NAME, 'MechEdit');
+define(COMPANY_LOGO, 'images/logo.gif');
+define(VERSION_STRING, 'Version 1.0 Alpha');
 
-	define(COMPANY_NAME,'MechEdit');
+// Language
+define(LANGUAGE_DIR, 'languages/english/');
+include LANGUAGE_DIR.'language.php';
 
-	define(COMPANY_LOGO,'images/logo.gif');
+// Software Configuration
 
-	define(VERSION_STRING,'Version 1.0 Alpha');
+define(CSS_TAG, 'clientedit');
 
-	// Language
-
-	define(LANGUAGE_DIR,'languages/english/');
-
-	include LANGUAGE_DIR.'language.php';
-
-
-
-	// Software Configuration
-
-	define(CSS_TAG,'clientedit');
-
-	
-
-  // Strongly suggested this not be publically accessable directory!!!!!!!
-
-	define(DATA_DIR,'data/');
-
-
-
-	
+// Strongly suggested this not be publically accessable directory!!!!!!!
+define(DATA_DIR, 'data/');
 
 ?>
-
--- trunk/1.0 Beta/themes/basic/footer.php
+++ trunk/1.0 Beta/themes/basic/footer.php
@@ -1,5 +1,4 @@
 <?php
-
 /*******************************************************************************
  Mech Edit Copyright 2009 Robert W. Mech
  ($Rev: 0 $)
@@ -9,17 +8,13 @@
  MIT and GPL licenses. This means that you can choose the license that best suits
  your project, and use it accordingly.
  Redistributions of files must retain the above copyright notice.
-
 *******************************************************************************/
-
 ?>
-<div id="footer">
-    <?= COMPANY_NAME ?>
-    <a href="http://mechedit.mechsoftware.com/">
-        <?= TXT_POWEREDBY ?>
-    </a>
-    | 
-    <?= VERSION_STRING ?>
+<div id="footer">    
+  <?= COMPANY_NAME ?>    
+  <a href="http://mechedit.mechsoftware.com/">        
+    <?= TXT_POWEREDBY ?>    </a>    |      
+  <?= VERSION_STRING ?>
 </div>
 </div>
 <script>
