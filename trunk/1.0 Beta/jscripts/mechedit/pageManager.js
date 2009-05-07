aff5f20593ab233c32f3b18ad6d8b9fccead4666
rwmech <rwmech@520e625c-3a4e-11de-97fb-aff7d2a6cc54>
2009-05-07 19:12:58 +0000

    Updated Website Address

--- trunk/1.0 Beta/jscripts/mechedit/pageManager.js
+++ trunk/1.0 Beta/jscripts/mechedit/pageManager.js
@@ -1,47 +1,93 @@
-/*******************************************************************************
-Mech Edit Copyright 2009 Robert W. Mech
- ($Rev: 0 $)
-Website: http://mechedit.mechsoftware.com/
-Author: Robert W. Mech
-
-Mech Edit is available for use in all personal or commercial projects under both 
-MIT and GPL licenses. This means that you can choose the license that best suits 
-your project, and use it accordingly.
-
-Redistributions of files must retain the above copyright notice.
-*******************************************************************************/
-
-function addPage(){
-	if(confirm(addPageText)){
-		var queryString='';
-		// Build Data
-		$('.pagefield').each(function(){  
-			  queryString+="&"+this.id+"="+this.value; 
-		 });
-		 // Send Data 
-		$.getJSON("pageManager.php?action=add"+queryString+'&id='+$('#FLD_id').val(),function(data){  
-		  // Display Response to transmission
-		   alert(data.status);  
-		   listPages();
-	   	   $('.pagefield').val('');
-		 });
-	}
-}
-function removePage(pageKey){
-	if (confirm(removePageText)) {
-		$.getJSON("pageManager.php?action=remove&key=" + pageKey, function(data){
-			alert(data.status)
-		   listPages();
-		});
-	}
-}
-function listPages(){
-	    $.getJSON("pageManager.php?action=list&id="+$('#FLD_id').val()+"&_rn=" + Math.random(0, 10000), function(data){
-			var html="";
-			for(i=0;i<data.length;i++){
-			html += '<tr><td>'+data[i].title+'</td><td>'+data[i].path+'</td><td><div class="clickField" onclick="removePage(\''+data[i].key+'\')">'+deletePageText+'</div></td></tr>';
-			}
-			$('#activePages>tbody').html(html);
-		});
-
+/*******************************************************************************
+
+Mech Edit Copyright 2009 Robert W. Mech
+
+ ($Rev: 0 $)
+
+Website: http://www.mechedit.com/
+
+Author: Robert W. Mech
+
+
+
+Mech Edit is available for use in all personal or commercial projects under both 
+
+MIT and GPL licenses. This means that you can choose the license that best suits 
+
+your project, and use it accordingly.
+
+
+
+Redistributions of files must retain the above copyright notice.
+
+*******************************************************************************/
+
+
+
+function addPage(){
+
+	if(confirm(addPageText)){
+
+		var queryString='';
+
+		// Build Data
+
+		$('.pagefield').each(function(){  
+
+			  queryString+="&"+this.id+"="+this.value; 
+
+		 });
+
+		 // Send Data 
+
+		$.getJSON("pageManager.php?action=add"+queryString+'&id='+$('#FLD_id').val(),function(data){  
+
+		  // Display Response to transmission
+
+		   alert(data.status);  
+
+		   listPages();
+
+	   	   $('.pagefield').val('');
+
+		 });
+
+	}
+
+}
+
+function removePage(pageKey){
+
+	if (confirm(removePageText)) {
+
+		$.getJSON("pageManager.php?action=remove&key=" + pageKey, function(data){
+
+			alert(data.status)
+
+		   listPages();
+
+		});
+
+	}
+
+}
+
+function listPages(){
+
+	    $.getJSON("pageManager.php?action=list&id="+$('#FLD_id').val()+"&_rn=" + Math.random(0, 10000), function(data){
+
+			var html="";
+
+			for(i=0;i<data.length;i++){
+
+			html += '<tr><td>'+data[i].title+'</td><td>'+data[i].path+'</td><td><div class="clickField" onclick="removePage(\''+data[i].key+'\')">'+deletePageText+'</div></td></tr>';
+
+			}
+
+			$('#activePages>tbody').html(html);
+
+		});
+
+
+
 }
--- trunk/1.0 Beta/themes/basic/header.php
+++ trunk/1.0 Beta/themes/basic/header.php
@@ -1,30 +1,59 @@
-<?php
-/*******************************************************************************
-Mech Edit Copyright 2009 Robert W. Mech
- ($Rev: 0 $)
-Website: http://mechedit.mechsoftware.com/
-Author: Robert W. Mech
-
-Mech Edit is available for use in all personal or commercial projects under both 
-MIT and GPL licenses. This means that you can choose the license that best suits 
-your project, and use it accordingly.
-
-Redistributions of files must retain the above copyright notice.
-*******************************************************************************/
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-  <head>
-  <meta http-equiv="content-type" content="text/html; charset=windows-1250">
-  <title><?=COMPANY_NAME?> <?=TXT_POWEREDBY?></title>
-  <link href="themes/basic/style.css" media="all" rel="stylesheet" type="text/css">
-  <?php scriptIncludes(); ?>
-  </head>
-  <body>
-  	<div id="wrapper">
-	  <div id="header">
-	    <h1><?=COMPANY_NAME?></h1>
-	    <div id="topnav">
-			<?php buildLinks(); ?>
-	    </div>
+<?php
+
+/*******************************************************************************
+
+Mech Edit Copyright 2009 Robert W. Mech
+
+ ($Rev: 0 $)
+
+Website: http://www.mechedit.com/
+
+Author: Robert W. Mech
+
+
+
+Mech Edit is available for use in all personal or commercial projects under both 
+
+MIT and GPL licenses. This means that you can choose the license that best suits 
+
+your project, and use it accordingly.
+
+
+
+Redistributions of files must retain the above copyright notice.
+
+*******************************************************************************/
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+
+  <head>
+
+  <meta http-equiv="content-type" content="text/html; charset=windows-1250">
+
+  <title><?=COMPANY_NAME?> <?=TXT_POWEREDBY?></title>
+
+  <link href="themes/basic/style.css" media="all" rel="stylesheet" type="text/css">
+
+  <?php scriptIncludes(); ?>
+
+  </head>
+
+  <body>
+
+  	<div id="wrapper">
+
+	  <div id="header">
+
+	    <h1><?=COMPANY_NAME?></h1>
+
+	    <div id="topnav">
+
+			<?php buildLinks(); ?>
+
+	    </div>
+
 	  </div>
