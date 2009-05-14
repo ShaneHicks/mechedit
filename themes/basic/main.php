38310e6bcb7cf2967909cb16e6f411f7b981e17f
rwmech <rwmech@520e625c-3a4e-11de-97fb-aff7d2a6cc54>
2009-05-14 21:10:36 +0000

    Moved remotely

---
+++ themes/basic/main.php
@@ -0,0 +1,15 @@
+<?php
+/*******************************************************************************
+Mech Edit Copyright 2009 Robert W. Mech
+ ($Rev: 0 $)
+Website: http://www.mechedit.com/
+Author: Robert W. Mech
+Mech Edit is available for use in all personal or commercial projects under both 
+MIT and GPL licenses. This means that you can choose the license that best suits 
+your project, and use it accordingly.
+Redistributions of files must retain the above copyright notice.
+*******************************************************************************/
+?>
+  <div id="content">
+  <?php mainContent(); ?>
+  </div>
